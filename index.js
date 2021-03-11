e9068be893c643009539cfce8c556e0f321f42cb
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-11 10:43:32 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+require('dotenv').config()
 console.log('woot')
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+console.log(process.env.NODE_ENV)
+
+//We only want cors in Developement this time, this is going to be full stack app, so online will be from same location. In dev, it's different ports
+// on Heroku machine, an env variable is called "NODE_ENV" --> "production" or "devlopment"
+if (process.env.NODE_ENV === 'devlopment') { 
+  const cors = require('cors') 
+  server.use(cors())
+}
